49673bddd80ad62f96c5658eebd2e5d43bb3b978
Yew Kang Wei <yewyew6933@gmail.com>
2020-06-15 19:06:56 +0800

    Updates

---
+++ precache-manifest.3fb8c3ac331e09624cb2bbc46b9b2015.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d00a6eff4548e1ccccb346b7f9c9fa13",
+    "url": "/Pokemon-App/index.html"
+  },
+  {
+    "revision": "75de0474807233644cd8",
+    "url": "/Pokemon-App/static/css/main.f617fdd1.chunk.css"
+  },
+  {
+    "revision": "e1a9009cd424602cef40",
+    "url": "/Pokemon-App/static/js/2.ddc364d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon-App/static/js/2.ddc364d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75de0474807233644cd8",
+    "url": "/Pokemon-App/static/js/main.fa8f43d8.chunk.js"
+  },
+  {
+    "revision": "e73fbca6ba3e4e01e52f",
+    "url": "/Pokemon-App/static/js/runtime-main.580451a4.js"
+  }
+]);
