ad635501386627ce8af5810c32dc191afc28ee1e
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-12 10:44:57 +0800

    add datalist and search button

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,17 +1,38 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 export default function SearchPage() {
+  const [pokemonNames, setPokemonNames] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const searchElRef = useRef();
+
+  function handleSearch() {
+    console.log(searchElRef.current.value);
+  }
+
   useEffect(() => {
     // get all pokemon names
     axios
       .get("https://pokeapi.co/api/v2/pokemon?limit=1000&offset=0")
       .then((res) => {
         const pokemonNameList = res.data.results.map((result) => result.name);
-        console.log(pokemonNameList);
+        setPokemonNames(pokemonNameList);
+        setIsLoading(false);
       })
       .catch((err) => console.log(err));
   }, []);
 
-  return <div>hi</div>;
+  if (isLoading) return <h1>Loading</h1>;
+
+  return (
+    <div>
+      <input type="search" list="pokemonNames" ref={searchElRef} />
+      <button onClick={handleSearch}>Search</button>
+      <datalist id="pokemonNames">
+        {pokemonNames.map((pokemonName) => (
+          <option value={pokemonName} key={pokemonName} />
+        ))}
+      </datalist>
+    </div>
+  );
 }
