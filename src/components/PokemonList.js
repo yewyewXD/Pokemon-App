1f6d8a536a4dc10f50bce744b7afd1aa9610528d
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-15 12:19:51 +0800

    send name to context

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,12 +1,28 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+
+import NameContext from "../context/name-context";
 
 export default function PokemonList({ pokemons }) {
+  const history = useHistory();
+  const contextType = useContext(NameContext);
+
+  function handlePostName(name) {
+    contextType.setName(name);
+    history.push("/search");
+  }
+
   return (
     <div className="container">
       <div className="pokemon-list d-flex justify-content-around align-items-center flex-wrap">
         {pokemons.map((pokemon) => (
-          <div className="pokemon card m-sm-5 m-1" key={pokemon.name}>
+          <div
+            className="pokemon card m-sm-5 m-1"
+            key={pokemon.name}
+            onClick={() => {
+              handlePostName(pokemon.name);
+            }}
+          >
             <img
               className="card-img-top w-75 mx-auto"
               src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
