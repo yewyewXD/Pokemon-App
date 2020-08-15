e11605d7a1798ec5780da70efafbc260cf9f6cbb
Yew Kang Wei <yewyew6933@gmail.com>
2020-08-15 12:16:35 +0800

    add NameContext provider

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import PokemonPage from "./pages/PokemonPage";
 import SearchPage from "./pages/SearchPage";
+import NameContext from "./context/name-context";
 
 function App() {
+  const [pokemonName, setPokemonName] = useState(null);
+  function handleSetName(name) {
+    setPokemonName(name);
+  }
+
   return (
-    <Router>
-      <Switch>
-        <Redirect from="/Pokemon-App" to="/" exact />
-        <Route path="/" component={PokemonPage} exact />
-        <Route path="/search" component={SearchPage} />
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <NameContext.Provider
+        value={{
+          pokemonName: pokemonName,
+          setName: handleSetName,
+        }}
+      >
+        <Switch>
+          <Redirect from="/Pokemon-App" to="/" exact />
+          <Route path="/" component={PokemonPage} exact />
+          <Route path="/search" component={SearchPage} />
+        </Switch>
+      </NameContext.Provider>
+    </BrowserRouter>
   );
 }
 export default App;
