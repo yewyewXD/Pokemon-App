d3d27776d866d29108a3524c59f18fd2394c1170
Yew Kang Wei <yewyew6933@gmail.com>
2020-09-18 21:17:07 +0800

    add landing page

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "../components/Global/Header";
+
+export default function LandingPage() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
