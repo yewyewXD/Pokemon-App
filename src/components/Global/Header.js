ec2854ee33a6e9d4d1bc9bc27914ac279100a7e8
Yew Kang Wei <yewyew6933@gmail.com>
2020-09-19 17:02:15 +0800

    init hero slider

--- src/components/Global/Header.js
+++ src/components/Global/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 export default function Header() {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
+    <Navbar variant="dark" className="fixed-top">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="ml-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Container>
     </Navbar>
   );
 }
--- src/components/Landing/Hero.js
+++ src/components/Landing/Hero.js
@@ -3,26 +3,45 @@ import Slider from "react-slick";
 
 export default function Hero() {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
+    fade: true,
   };
 
   return (
     <>
       <Slider className="slider" {...settings}>
         <div className="slide slide1">
-          <h3>1</h3>
+          <div className="slide-overlay w-100 h-100 all-center">
+            <div className="slide-caption text-white text-center">
+              <h1>Gotta catch them all</h1>
+              <p>In YewPokedex, we cover all pokemon under the hood</p>
+              <button className="btn btn-primary btn-lg mt-4">Get them</button>
+            </div>
+          </div>
         </div>
         <div className="slide slide2">
-          <h3>2</h3>
+          <div className="slide-overlay w-100 h-100 all-center">
+            <div className="slide-caption text-white text-center">
+              <h1>Gotta catch them all</h1>
+              <p>In YewPokedex, we cover all pokemon under the hood</p>
+              <button className="btn btn-primary btn-lg mt-4">Get them</button>
+            </div>
+          </div>
         </div>
         <div className="slide slide3">
-          <h3>3</h3>
+          <div className="slide-overlay w-100 h-100 all-center">
+            <div className="slide-caption text-white text-center">
+              <h1>Gotta catch them all</h1>
+              <p>In YewPokedex, we cover all pokemon under the hood</p>
+              <button className="btn btn-primary btn-lg mt-4">Get them</button>
+            </div>
+          </div>
         </div>
       </Slider>
     </>
